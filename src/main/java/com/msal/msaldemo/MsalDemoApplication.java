91cfc084150042a7720702f92c694999a9ff621f
PurushottamDeshmukh <purushottamdeshmukh@PurushottamDeshmukhs-MacBook-Pro.local>
2023-11-28 15:04:08 +0530

    f

---
+++ src/main/java/com/msal/msaldemo/MsalDemoApplication.java
@@ -0,0 +1,13 @@
+package com.msal.msaldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsalDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsalDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msal/msaldemo/MsalDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msal.msaldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsalDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
